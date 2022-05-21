cd3bb696379ea5a33d0c8d3c163a14988a7efa8c
yishyu <yishen.yu@ulb.be>
2022-05-21 11:35:01 +0200

    [Del] unneeded columns

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import glob
 import platform
-import numpy as np 
-
 
 def get_all_dataframes(path):
     os_char = {'Linux': '/', 'Darwin': '/', 'Windows': '\\'}
@@ -30,18 +28,16 @@ def select_columns_from_files():
     kept_columns = {
         "info": ["id", "name", "general_position", "position", "height", "weight", "nt", "countryob", "club", "age"],
         "misc": same_columns + ["cards_yellow", "cards_red", "fouls", "offsides", "interceptions", "tackles_won", "ball_recoveries"],
-        "gca": same_columns + ["gca", "gca_per90"],
         "defense": same_columns + ["tackles", "pressures", "dribbled_past", "blocks", "blocked_shots", "pressure_regain_pct", "tackles_won"],
         "keeper": same_columns + ["goals_against_gk", "goals_against_per90_gk", "shots_on_target_against", "save_pct", "clean_sheets", "clean_sheets_pct", "pens_att_gk", "pens_missed_gk", "pens_save_pct"],
-        "keeper_adv": same_columns + ["passes_gk", "pct_passes_launched_gk"],
         "passing": same_columns + ["passes", "passes_pct", "passes_short", "passes_pct_short", "passes_medium", "passes_pct_medium", "passes_long", "passes_pct_long", "assists"],
         "playing_time": same_columns + ["games", "minutes", "minutes_per_game", "points_per_match"],
         "shooting": same_columns + ["goals", "shots_total", "shots_on_target", "shots_on_target_pct", "goals_per_shot", "goals_per_shot_on_target"],
     }
-    
+
     for name, columns in kept_columns.items():
         if name == "info":
-            all_df[name]["general_position"] = all_df[name]["position"].apply(split_position)   
+            all_df[name]["general_position"] = all_df[name]["position"].apply(split_position)
         create_new_csv(name, all_df[name], columns)
 
 if __name__ == "__main__":
